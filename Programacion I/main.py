57b0d7b5234667f4483eddc788ee7d7239cd2675
Agus <agustin15camporeale@gmail.com>
2024-12-13 14:40:52 -0300

    ADD:Ejercicio:Integrador opcion 7

--- Programacion I/main.py
+++ Programacion I/main.py
@@ -11,7 +11,8 @@ def main():
     4. Listar inventario (mostrar productos)
     5. Actualizar el inventario (cuando un producto se vende (disminuir su cantidad))
     6. Escribir un archivo (con el inventario actualizado)
-    7. Salir
+    7. Mappear precios(Aumentarlos un 10%)
+    8. Salir
     \nSeleccione una opción: """))
         if opcion == 1:
             inventario = leer_archivo("inventario")
@@ -20,20 +21,25 @@ def main():
         if opcion == 2 and leer == True:
             buscar_producto(inventario,"almedras")
         if opcion == 3 and leer == True:
-            ordenar_inventario(inventario, "cantidad")
+            criterio = input("Ingrese el criterio de ordenación (Precio/Cantidad): ")
+            ordenar_inventario(inventario, criterio)
         if opcion == 4 and leer == True:
             listar_inventario(inventario)            
         if opcion == 5 and leer == True:
-            actualizar_cantidad(inventario,"atun en lata", 100)
+            nombre_producto = input("Ingrese el nombre del producto: ").lower()
+            cantidad_vendida = int(input("Ingrese la cantidad vendida: "))
+            actualizar_cantidad(inventario,nombre_producto, cantidad_vendida)
         if opcion == 6 and leer == True:
             escribir_archivo(inventario, "ventas")
-        if opcion == 7:
+        if opcion == 7 and leer == True:
+            mappear_precios(inventario)
+        if opcion == 8:
             continuar = "n"
             leer = True
             print("Fin del programa.")
         if leer == False:
             print("\nNo se ha leido el archivo, Volviendo al menú...")
-        while opcion > 7 or opcion < 1:
+        while opcion > 8 or opcion < 1:
             opcion = int(input("Rango invalido, Vuelva a seleccionar una opción: "))
                
 main()
